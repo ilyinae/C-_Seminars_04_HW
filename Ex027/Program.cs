7e3728abb042a2a7fdf245da7d87f7adb285f58d
Anton Ilyin <ilyin.a.e@gmail.com>
2022-09-25 12:14:12 +0300

    Added Ex027

---
+++ Ex027/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+string A = Console.ReadLine();
+int summ = 0;
+
+for (int i=0; i< A.Length; i++)
+    summ = summ + Convert.ToInt32(Convert.ToString(A[i]));
+Console.WriteLine($"Сумма цифр числа {A} равна {summ}");
