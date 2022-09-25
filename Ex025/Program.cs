a76b60e2e0f897c29cab6389536a8a7f960afa65
Anton Ilyin <ilyin.a.e@gmail.com>
2022-09-25 11:53:58 +0300

    Added Ex025

---
+++ Ex025/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = -1;
+int aPow = 1;
+while (B < 0 )
+{
+    Console.WriteLine("Введите число B: ");
+    B = Convert.ToInt32(Console.ReadLine());
+}    
+for (int i = 1; i <= B; i++) aPow = A*aPow;
+Console.WriteLine ($"{A}^{B} = {+aPow}");
